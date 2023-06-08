3a03715cd3b2e31c2dd108db3b7543b6e71b5ab2
ama-gha <135840868+ama-gha@users.noreply.github.com>
2023-06-09 03:29:47 +0900

    ベータ版基礎のためのファイル更新
    index HTMLに合わせて更新

---
+++ main.js
@@ -0,0 +1,26 @@
+const twitterButtonOfAva = document.getElementsByClassName("twitter");
+const instagramButtonOfAva = document.getElementsByClassName("instagram");
+const facebookButtonOfAva = document.getElementsByClassName("facebook");
+const tiktokButtonOfAva = document.getElementsByClassName("tiktok");
+
+const twitterButtonOfOfficial = document.getElementsByClassName("twitter_official");
+
+function jumpTwitterOfAva() {
+    location.href = 'https://twitter.com/AvaMax';
+}
+
+function jumpInstagramOfAva() {
+    location.href = 'https://www.instagram.com/avamax/';
+}
+
+function jumpFacebookOfAva() {
+    location.href = 'https://www.facebook.com/AvaMax';
+}
+
+function jumpTiktokOfAva() {
+    location.href = 'https://www.tiktok.com/@avamax';
+}
+
+function jumpTwitterOfOfficial(){
+    location.href = 'https://twitter.com/AvaMax_JP';
+}
